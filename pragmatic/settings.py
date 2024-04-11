7aa0e77979af80d410d324402562cd3b4550b6fe
maengjh0208 <juhee.bda@gmail.com>
2024-04-11 22:37:24 +0900

    환경파일(.env) 생성 및 .gitignore 파일 추가

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -11,16 +11,27 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# reading .env file
+environ.Env.read_env(
+    env_file=os.path.join(BASE_DIR, '.env')
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d@#4$=g@9r36d7*tub8&qsq#&z9twx#bhntk1@#3ay#&b5=@wa'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
