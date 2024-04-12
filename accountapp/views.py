01a9b94d7a7a1731058213fe2f3e0e683ebd4246
maengjh0208 <juhee.bda@gmail.com>
2024-04-12 21:44:32 +0900

    template 생성

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def hello_word(request):
-    return HttpResponse('hello world')
+    return render(request, 'base.html')
