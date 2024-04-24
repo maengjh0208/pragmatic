ecd11c8d328874ee31cfd5cffbc21ffe337ffcfb
maengjh <juhee.bda@gmail.com>
2024-04-24 21:42:32 +0900

    비밀번호 변경 기능 구현

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth.views import PasswordChangeView
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
@@ -34,3 +35,9 @@ class AccountDetailView(DetailView):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+
+
+class AccountUpdateView(PasswordChangeView):
+    model = User
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/update.html'
