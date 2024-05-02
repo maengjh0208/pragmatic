89ecd9fcb4b64c3fb64101412f9dfe7291785651
maengjh <juhee.bda@gmail.com>
2024-05-02 16:48:24 +0900

    bug fix

--- accountapp/views.py
+++ accountapp/views.py
@@ -45,5 +45,6 @@ class AccountUpdateView(PasswordChangeView):
 
 class AccountDeleteView(DeleteView):
     model = User
+    context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:login')
     template_name = 'accountapp/delete.html'
